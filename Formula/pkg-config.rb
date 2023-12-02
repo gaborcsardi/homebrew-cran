1f0d46060ecaea3a96cfb482fd2715f42a877a27
Gábor Csárdi <csardi.gabor@gmail.com>
2023-12-02 10:44:03 +0100

    pkg-config: drop revision, drop broken URL

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -3,10 +3,8 @@ class PkgConfig < Formula
   homepage "https://freedesktop.org/wiki/Software/pkg-config/"
   url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
   mirror "http://fresh-center.net/linux/misc/pkg-config-0.29.2.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
   license "GPL-2.0-or-later"
-  revision 3
 
   livecheck do
     url "https://pkg-config.freedesktop.org/releases/"

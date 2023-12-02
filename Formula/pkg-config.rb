856db554a4f4e516fb2f669b519153db013465e1
Gábor Csárdi <csardi.gabor@gmail.com>
2023-12-02 12:15:58 +0100

    Revert "Increase pkg-config revision"
    This reverts commit 0c5c9d8c72a77a5d21b59b2b9bd9f5003aa44648.

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -5,7 +5,6 @@ class PkgConfig < Formula
   mirror "http://fresh-center.net/linux/misc/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
   license "GPL-2.0-or-later"
-  revision 3
 
   livecheck do
     url "https://pkg-config.freedesktop.org/releases/"

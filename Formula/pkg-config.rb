fdcbe75cf0a8b5a68ee96f24654d49f3bfec8465
Gábor Csárdi <csardi.gabor@gmail.com>
2023-12-02 14:47:23 +0100

    pkg-config: try to use revision only for one bottle

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -13,9 +13,11 @@ class PkgConfig < Formula
 
   bottle do
     root_url "https://github.com/gaborcsardi/homebrew-cran/releases/download/pkg-config-0.29.2"
-    sha256 ventura:  "66b70f5fbab5395c9c3d307e349dec015320fec13027f55a07878c70752572a1"
-    sha256 monterey: "3992e9cd55debc4e9559dd8cc067c61cd2fd3783e72e5c2e077db4ce17023712"
-    sha256 big_sur:  "ddd5bc2cc2402d721e954d35d7a0f9aa2151ce9dde2784e6351b8cc849e29a85"
+    revision 3
+    sha256 arm64_big_sur: "e5f21c143b41c5b4a04c2081e8ea414cb20a703d10a458c9e9854ebde9581d04"
+    sha256 ventura:       "66b70f5fbab5395c9c3d307e349dec015320fec13027f55a07878c70752572a1"
+    sha256 monterey:      "3992e9cd55debc4e9559dd8cc067c61cd2fd3783e72e5c2e077db4ce17023712"
+    sha256 big_sur:       "ddd5bc2cc2402d721e954d35d7a0f9aa2151ce9dde2784e6351b8cc849e29a85"
   end
 
   conflicts_with "pkgconf", because: "both install `pkg.m4` file"
